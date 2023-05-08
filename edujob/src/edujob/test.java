06760f5c0e9bc9936d1272572a7ad047634dc592
rathodrolex <sinhavikram728@gmail.com>
2023-05-08 12:40:57 +0530

    generation of india

--- edujob/src/edujob/test.java
+++ edujob/src/edujob/test.java
@@ -4,7 +4,7 @@ public class test {
 	
 	class Simple{
 		  public static void main(String args[]){
-		    System.out.println("Hello 2coms");
+		    System.out.println("Hello generations of india");
 		  }
 		}
 
